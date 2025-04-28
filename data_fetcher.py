72871d5acd58f3043e7ae1dae7cbfaf2a3851d3c
Neuronaut <patrec.ug@gmail.com>
2025-04-28 04:16:40 +0200

    corrected data fetcher

--- data_fetcher.py
+++ data_fetcher.py
@@ -4,20 +4,13 @@ import requests
 
 X_API_KEY = "gmEqTVp4GLaYcXUIJLblRw==SQdua3MhJo7y3LkG"
 REQUEST_ANIMALS = f"https://api.api-ninjas.com/v1/animals?name=<ANIMAL_NAME>&X-Api-Key={X_API_KEY}"
-
+FILENAME = "animals_data.json"
 
 def get_user_input() -> str:
     """Asks user to enter the name of an animal"""
     animal_name = input("Enter a name of an animal: ")
     request_url = REQUEST_ANIMALS.replace("<ANIMAL_NAME>", animal_name)
     return animal_name, request_url
-
-    animal_name, request_url = get_user_input()
-
-
-def fetch_data():
-    raw = requests.get(request_url)
-    animals_data = raw.json()
 
 
 def load_data(filename = FILENAME):
@@ -27,10 +20,16 @@ def load_data(filename = FILENAME):
     return data
 
 
-def save_data(highscore_dict, filename = FILENAME):
+def save_data(animals_data, filename = FILENAME):
     """Writes the updated highscore data to a JSON file."""
     with open(filename, 'w', encoding='utf-8') as fileobj:
-        json.dump(highscore_dict, fileobj,  ensure_ascii=False, indent = 4)
+        json.dump(animals_data, fileobj,  ensure_ascii=False, indent = 4)
 
-    print(f"The highscores were saved successfully in {filename}.")
+
+def fetch_data():
+    animal_name, request_url = get_user_input()
+    raw = requests.get(request_url)
+    animals_data = raw.json()
+    save_data(animals_data, FILENAME)
+    return animal_name
 
