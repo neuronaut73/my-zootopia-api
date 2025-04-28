654583a97f836bc334a1a77ef0489f5103c65510
Neuronaut <patrec.ug@gmail.com>
2025-04-28 02:47:23 +0200

    Added user input to chose animals name. Added "Website generated" at end

--- ninja.py
+++ ninja.py
@@ -4,9 +4,7 @@ import requests
 import json
 
 
-name = "fox"
-
-REQUEST_ANIMALS = f"https://api.api-ninjas.com/v1/animals?name={name}&X-Api-Key={X_API_KEY}"
+REQUEST_ANIMALS = f"https://api.api-ninjas.com/v1/animals?name=<ANIMAL_NAME>&X-Api-Key={X_API_KEY}"
 
 
 def save_output(output:dict, filename: str):
@@ -14,25 +12,23 @@ def save_output(output:dict, filename: str):
         handle.write(str(output))
 
 
+def get_user_input() -> str:
+    """Asks user to enter the name of an animal"""
+    animal_name = input("Enter a name of an animal: ")
+    request_url = REQUEST_ANIMALS.replace("<ANIMAL_NAME>", animal_name)
+    return animal_name, request_url
+
+
 def main():
-    raw = requests.get(REQUEST_ANIMALS)
+    animal_name, request_url = get_user_input()
+
+    raw = requests.get(request_url)
     text = raw.json()
     print(text)
     for item in text:
         print(item["name"])
-    # animals = text["countries"]
-    # for country in countries:
-    #     print(f"{country["name"]} ({country["codes"]["alpha-3"]})")
-    # land = input("Please enter a country: ")
-    # REQUEST_URL = f"https://holidayapi.com/v1/holidays?pretty&key={API_KEY}&format=json&year={year}&country={land}"
-    # raw2 = requests.get(REQUEST_URL)
-    # text2 = raw2.json()
 
-    # print(text2)
-    # print(f"List of holidays in the last year ({year}):")
-
-    # for item in text2["holidays"]:
-    #     print(f"{item["name"]} ({item["weekday"]["date"]["name"]}, {item["date"]})")
+    print(f"{animal_name}-Website was successfully generated to the file animals.html.")
 
 
 if __name__ == "__main__":
